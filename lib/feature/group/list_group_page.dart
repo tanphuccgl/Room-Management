4efe60f8470b2f42b54d23d1fa46e8edf3c23eb2
Levi <phamnguyentanphucgl@gmail.com>
2023-06-24 18:07:13 +0700

    update name folder

--- lib/feature/group/list_group_page.dart
+++ lib/feature/group/list_group_page.dart
@@ -1,5 +1,6 @@
+import 'package:app_qlphongtro_sv/feature/group/group_bloc.dart';
 import 'package:app_qlphongtro_sv/feature/phongtro/ds_phongtro.dart';
-import 'package:app_qlphongtro_sv/feature/room/group_bloc.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
